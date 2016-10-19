0bfd203aa6daa72765321a37e6bfa7af17e4bd0e
Daniel T. Lee <danieltimlee@gmail.com>
2016-10-19 17:36:23 +0900

    Changed Layout Design

--- app/routes/index.py
+++ app/routes/index.py
@@ -6,11 +6,11 @@ from app.routes.oauth import get_oauth_url
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
-    return render_template('pages/index/index.html',
+    return render_template('pages/site/index.html',
                            signin=get_oauth_url())
 
 
 @app.route("/about")
 def about():
-    return render_template('pages/about.html',
+    return render_template('pages/site/about.html',
                            signin=get_oauth_url())
--- app/static/js/util.js
+++ app/static/js/util.js
@@ -2,6 +2,7 @@ var clear = function () {
   $('.nag').nag('hide');
 };
 $('.ui.nav.sidebar').sidebar('attach events', '.ui.top.menu > .hamburger.item');
+$('.ui.notification.sidebar').sidebar('attach events', '.ui.top.menu > .user.menu > .notification.item');
 
 $('.dropdown').dropdown();
 $('.nag').nag('show');
