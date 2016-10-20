843be4ac6ab115ee441dce230ebdb308d1d559c6
Daniel T. Lee <danieltimlee@gmail.com>
2016-10-20 20:40:51 +0900

    Form changed to FlaskForm
    Flask_WTF updated 0.13.1

--- app/forms/user.py
+++ app/forms/user.py
@@ -1,9 +1,11 @@
-from wtforms import StringField, PasswordField, Form
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField
 from wtforms.fields.html5 import EmailField
-from wtforms.validators import DataRequired, Length, required, EqualTo
+from wtforms.validators import Length, required, EqualTo
 from wtforms_components import ModelForm
 
 
+# TODO: 가능하면 FlaskForm으로 모두 바꿔줄 수 있게.
 class ModifyProfileForm(ModelForm):
     username = StringField('ID', [])
     password = PasswordField('Password', [])
@@ -28,6 +30,12 @@ class ModifyPasswordForm(ModelForm):
     ])
 
 
-class VerifymeForm(Form):
-    username = StringField('ID', [DataRequired()])
-    password = PasswordField('Password', [DataRequired()])
+class VerifymeForm(FlaskForm):
+    username = StringField('ID', [
+        required(message='비밀번호는 필수 항목입니다.'),
+        Length(min=4, max=15, message='%(min)d 이상 %(max)d 이하로 입력해주세요.')
+    ])
+    password = PasswordField('Password', [
+        required(message='비밀번호는 필수 항목입니다.'),
+        Length(min=6, max=20, message='%(min)d 이상 %(max)d 이하로 입력해주세요.')
+    ])
