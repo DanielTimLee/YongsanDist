f8611e884dfaaeaea7de8b5cf1ee9367443662e1
Daniel T. Lee <danieltimlee@gmail.com>
2016-10-29 21:47:53 +0900

    Fabric 추가됨

---
+++ fabfile.py
@@ -0,0 +1,52 @@
+from fabric.api import *
+
+env.hosts = ['45.76.111.17']
+env.user = 'root'
+env.password = '%9yQ$@9@sR!V(_VT'
+env.activate = 'source venv/bin/activate && source .env'
+
+
+def install():
+    run('rm -rf YongsanDist')
+    run('git clone https://github.com/DanielTimLee/YongsanDist')
+    with cd('~/YongsanDist/'):
+        run('virtualenv --python=python3 venv')
+        with prefix(env.activate):
+            run('pip install -r requirements.txt')
+            run('bower install --allow-root')
+            run('python server.py')
+
+
+def deploy():
+    with cd('~/YongsanDist/'):
+        run('git checkout preview')
+        run('git pull origin preview')
+        with prefix(env.activate):
+            run('pip install -r requirements.txt')
+            run('bower install --allow-root')
+
+
+def register_upstart():
+    sudo('rm -f /etc/init/yongsandist.service')
+    sudo('ln -s /home/ubuntu/yongsandist/yongsandist.upstart.service /etc/init/yongsandist.upstart')
+    sudo('initctl reload-configuration')
+
+
+def start():
+    sudo('initctl start yongsandist.upstart')
+
+
+def stop():
+    sudo('initctl stop yongsandist.upstart')
+
+
+def status():
+    sudo('initctl status yongsandist.upstart')
+
+
+def proxy_start():
+    sudo('service nginx start')
+
+
+def proxy_stop():
+    sudo('service nginx stop')
